e762924ec5933c75014f9d296b54254793ff33f0
Rama <rama.alshaban@gmail.com>
2022-04-22 04:30:41 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(divide){
+    return 2000 / 100
+    }
+    // const square = function(){}
+    let x = 2
+    
+    const square = x => x*x
+    
+    const add = (a, b) => a + b
+    let a = 3
+    let b = 4
